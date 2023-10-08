b7a9b10c84c0f272d323233c7dcf380a4f2d602f
AmarMuric04 <muricamar2004@gmail.com>
2023-10-09 01:33:50 +0200

    Fixed some problems with 06-boolean.js.

--- 06 booleans/06-booleans.js
+++ 06 booleans/06-booleans.js
@@ -1,4 +1,4 @@
-// console.log(3 < 5); 
+// console.log(3 < 5);
 // console.log(3 > 5);
 
 /* if (0) {
@@ -36,7 +36,6 @@ console.log(message);
 const currency = undefined || 'USD' ;
 console.log(currency) */
 
-
 /* homework so that the computer picks a time and follows instructions on what to say.
   const name = 'Amar';
   const hour = Math.floor(Math.random()*24)
@@ -49,41 +48,27 @@ console.log(currency) */
     console.log(`Good Night, ${name}.`)
   } */
 
+/* Theres an amusement park, and everyone thats 6 or younger has a discount, same for people that are 65 and older, the oldest person in the world is 116 so if they say more than 116 they arent eligible, if its not the holidays, the discounts are available, if its not the holidays the discounts are available. holidays are June July and August. */
 
-/* Theres an amusement park, and everyone thats 6 or younger has a discount, same for people that are 65 and older, the oldest person in the world is 116 so if they say more than 116 they arent eligible, if its not the holidays, the discounts are available, if its not the holidays the discounts are available. holidays are June July and August. 
-let age = 0;
-  let isHoliday = +prompt('1-12');
+let isHoliday = +prompt("1-12");
 
-  if(isHoliday === 6 || isHoliday === 7 || isHoliday === 8) {
-    isHoliday = 'true';
-  } 
-  else {
-   isHoliday = 'false'
-  }
-
-if (isHoliday === 'true'){
-  console.log('its the holidays')
+if (isHoliday === 6 || isHoliday === 7 || isHoliday === 8) {
+  isHoliday = true;
+} else {
+  isHoliday = false;
 }
-  else if ((age <=6 || (age >= 65 && age <= 116)) && isHoliday === 'false') {
-    console.log('Its not the holidays');  
-    age = +prompt('Enter your age');
-    console.log('You are eligible for a discount.');
-  }
-   else if (age > 116 && isHoliday === 'false') {
-    console.log('Its not the holidays');  
-    age = +prompt('Enter your age');	
-    console.log('You would be older than the oldest person on the planet, try again.')
-  } 
-  
-  else if (age > 116 && isHoliday === 'true') {
-    console.log('Its not the holidays');  
-    age = +prompt('Enter your age');
-    console.log('You would be older than the oldest person on the planet, try again. + its the holidays.')
-  } 
-  else if ((age > 6 && age < 65) && isHoliday === 'false'){
-    console.log('Its not the holidays');  
-    age = +prompt('Enter your age');
-    console.log('You are not eligible for a discount.');
-  } */
+let Holiday = isHoliday ? "Its the holiday" : "Its not the holidays";
+console.log(Holiday);
 
- 
+if (isHoliday) {
+  age = +prompt("Enter age");
+  if (age <= 6 || (age >= 65 && age <= 116)) {
+    console.log("You are eligible for a discount.");
+  } else if (age > 116) {
+    console.log(
+      "You would be older than the oldest person on the planet, try again."
+    );
+  } else {
+    console.log("You are not eligible for a discount.");
+  }
+}
