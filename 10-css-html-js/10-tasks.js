b2bac2aedc4f99e2f957aaa13d181590a6529294
AmarMuric04 <muricamar2004@gmail.com>
2023-10-14 17:31:14 +0200

    Finished the last task for css/html/js.

--- 10-css-html-js/10-tasks.js
+++ 10-css-html-js/10-tasks.js
@@ -56,3 +56,32 @@ function thirdOnOffSingle() {
   }
   return thirdOnOff;
 }
+function checkThePrice() {
+  let input = Number(document.querySelector(".js-input").value);
+  let cost = document.querySelector("#js-cost");
+  if (input > 40) {
+    cost.classList.remove("redText");
+    cost.classList.remove("yellowText");
+    cost.textContent = `$${input}`;
+  } else if (input <= 40 && input !== 0 && input > 0) {
+    cost.classList.remove("redText");
+    cost.classList.remove("yellowText");
+    cost.textContent = `$${((input + 10) * 100) / 100}`;
+  } else if (input === 0) {
+    cost.classList.add("yellowText");
+    cost.textContent = "Cart is empty or the item is free.";
+  } else if (input < 0) {
+    cost.classList.add("redText");
+    cost.classList.remove("yellowText");
+    cost.textContent = "Error: cost cannot be less than $0";
+  } else {
+    cost.classList.add("redText");
+    cost.classList.remove("yellowText");
+    cost.textContent = "Error: The input you provided is not a number";
+  }
+}
+function enterKey(event) {
+  if (event.key === "Enter") {
+    checkThePrice();
+  }
+}
