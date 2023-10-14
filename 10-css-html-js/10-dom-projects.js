e38c86cdf9480cb2a6b2b6d6398d5057745abccb
AmarMuric04 <muricamar2004@gmail.com>
2023-10-14 03:15:17 +0200

    learned classList.add and remove, used it in code.

---
+++ 10-css-html-js/10-dom-projects.js
@@ -0,0 +1,31 @@
+const subscribe = document.querySelector(".js-subscribe-button");
+function subscribed() {
+  if (subscribe.innerText === "Subscribe") {
+    subscribe.textContent = "Subscribed";
+    subscribe.classList.add("subscribedNewClass");
+  } else {
+    subscribe.textContent = "Subscribe";
+    subscribe.classList.remove("subscribedNewClass");
+  }
+}
+
+function checkThePrice() {
+  let input = Number(document.querySelector(".js-input").value);
+  let cost = document.querySelector("#js-cost");
+  if (input > 40) {
+    cost.textContent = `$${input}`;
+  } else if (input <= 40 && input !== 0) {
+    cost.textContent = `$${((input + 10) * 100) / 100}`;
+  } else if (input === 0) {
+    cost.textContent = "Cart is empty or the item is free.";
+  } else {
+    cost.textContent = "Error";
+  }
+  document.getElementById("rock-paper-scissors-title").style.margin =
+    "21px 0px 16px 0px";
+}
+function enterKey(event) {
+  if (event.key === "Enter") {
+    checkThePrice();
+  }
+}
