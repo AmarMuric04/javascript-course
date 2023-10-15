9be41ebfbe091e9b1fb9f11b21cab679f17eb5ae
AmarMuric04 <muricamar2004@gmail.com>
2023-10-15 23:47:18 +0200

    Finished Array/Loop part1

--- 11-arrays-and-loops/11-todo-list.js
+++ 11-arrays-and-loops/11-todo-list.js
@@ -1,5 +1,15 @@
-const todolist = [];
+const todolist = ["make dinner", "wash dishes"];
+todoList();
 
+function todoList() {
+  let todolistHTML = "";
+  for (i = 0; i < todolist.length; i++) {
+    const todo = todolist[i];
+    const html = `<p>${todo}</p>`;
+    todolistHTML += html;
+  }
+  document.querySelector(".js-todo-list").innerHTML = `${todolistHTML}`;
+}
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
   let name = inputElement.value;
@@ -12,6 +22,7 @@ function onEnter(event) {
   if (event.key === "Enter") {
     addTodo();
   }
+  todoList();
 }
 
 function addTodoTwo() {}
