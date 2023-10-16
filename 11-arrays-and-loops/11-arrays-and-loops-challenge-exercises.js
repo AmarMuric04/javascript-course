f4964a0b97d6aa503ba1cbe369022a87f597ebe4
AmarMuric04 <muricamar2004@gmail.com>
2023-10-16 18:53:19 +0200

    Finished more exercises that i dont even undrstnd

--- 11-arrays-and-loops/11-arrays-and-loops-challenge-exercises.js
+++ 11-arrays-and-loops/11-arrays-and-loops-challenge-exercises.js
@@ -1,14 +1,47 @@
-const arr = [-5, 10, 3, 2, -9, 99];
-let object = { min: 0, max: 0 };
-let smallest = arr[0];
-let biggest = arr[0];
-for (let i = 1; i < arr.length; i++) {
-  if (arr[i] < smallest) {
-    smallest = arr[i];
-    object.min = smallest;
-  } else if (arr[i] > biggest) {
-    biggest = arr[i];
-    object.max = biggest;
+function minMax(array) {
+  const object = {
+    min: array[0],
+    max: array[0],
+  };
+  for (i = 0; i < array.length; i++) {
+    const value = array[i];
+    if (object.max === null || value < object.min) {
+      object.min = value;
+    }
+    if (object.max === null || value > object.max) {
+      object.max = value;
+    } else if (array.length === 1) {
+      object.max = array[0];
+      object.min = array[0];
+    }
   }
+  return object;
 }
-console.log(object);
+console.log(minMax([0, 2]));
+
+// function countWords(array) {
+//   const object = {};
+//   for (i = 0; i < array.length; i++) {
+//     const word = array[i];
+//     if (!object[word]) {
+//       object[word] = 1;
+//     } else {
+//       object[word]++;
+//     }
+//   }
+//   return object;
+// }
+
+function countWords(array) {
+  const repetitions = {};
+  for (i = 0; i < array.length; i++) {
+    let word = array[i];
+    if (!repetitions[word]) {
+      repetitions[word] = 1;
+    } else {
+      repetitions[word]++;
+    }
+  }
+  return repetitions;
+}
+console.log(countWords(["apple", "apple", "apple", "apple", "grape"]));
