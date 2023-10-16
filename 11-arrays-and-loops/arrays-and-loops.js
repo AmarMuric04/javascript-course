38f68a77a7ca0fecbbf25ab9cf3bebcf4d391e90
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2023-10-16 11:39:33 +0200

    Add files via upload

---
+++ 11-arrays-and-loops/arrays-and-loops.js
@@ -0,0 +1,57 @@
+// const nums = [10, 20, 30];
+// nums[2] = 99;
+// console.log(nums);
+
+// function getLastIndex(array) {
+//   for (i = 0; (i = array.length - 1); i++) {
+//     const lastNum = array[i];
+//     console.log(lastNum);
+//     return lastNum;
+//   }
+// }
+// //one way of geting the last index out of an array
+// function getLastIndexTwo(array) {
+//   let lastNum = array.length - 1;
+//   console.log(array[lastNum]);
+// }
+// //another way of geting the last index out of an array
+// function arraySwap(array) {
+//   let firstNum = array[0];
+//   let lastNum = array[array.length - 1];
+
+//   array[0] = lastNum;
+//   array[array.length - 1] = firstNum;
+
+//   console.log(array);
+// }
+// swap first and last thing in an array
+// for (i = 0; i <= 10; i += 2) {
+//   console.log(i);
+// }
+// for (i = 5; i >= 0; i--) {
+//   console.log(i);
+// }
+// let i = 0;
+// while (i <= 10) {
+//   console.log(i);
+//   i += 2;
+// }
+// let i = 5;
+// while (i >= 0) {
+//   console.log(i);
+//   i--;
+// }
+// let arr = [1, 2, 3];
+// let newArray = [];
+// for (i = 0; i < arr.length; i++) {
+//   newArray.push(arr[i] + 1);
+// }
+// console.log(newArray);
+
+// function addOne(array) {
+//   let newArray = [];
+//   for (i = 0; i < array.length; i++) {
+//     newArray.push(array[i] + 1);
+//   }
+//   console.log(newArray);
+// }
