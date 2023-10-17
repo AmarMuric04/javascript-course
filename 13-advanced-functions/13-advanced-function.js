4148decbc36f0d27d09625c72f10f78154d63014
AmarMuric04 <muricamar2004@gmail.com>
2023-10-17 11:47:36 +0200

    Async functions / Advanced functions

---
+++ 13-advanced-functions/13-advanced-function.js
@@ -0,0 +1,58 @@
+// function greeting() {
+//   console.log("hello");
+// }
+// greeting();
+
+// const num = 3;
+
+// const function1 = function () /*greeting*/ {
+//   console.log("hello2");
+// };
+
+// console.log(function1);
+// console.log(typeof function1);
+
+// //function is just another value, u can save it in a variable.
+
+// function1();
+
+// const object1 = {
+//   num: 2,
+//   fun: function greeting() {
+//     console.log("hello3");
+//   },
+// };
+// object1.fun();
+// function display(param) {
+//   console.log(param);
+// }
+
+// display(2);
+
+// function run(param) {
+//   param();
+// }
+// run(function () {
+//   console.log("hello4");
+// });
+//passing a function into another function
+// setTimeout(function () {
+//   console.log("timeout!");
+//   document.body.style.backgroundColor = "red";
+// }, 3000);
+
+// //THIS IS CALLED ASYNCHRONOUS CODE, CONSOLE WONT WAIT FOR IT TO LOAD
+
+// console.log("next line");
+
+// setInterval(function () {
+//   console.log("interval");
+// }, 3000);
+// console.log("next line2");
+["make dinner", "wash dishes", "watch youtube"].forEach(function (
+  value,
+  index
+) {
+  console.log(value);
+  console.log(index);
+});
---
+++ 13-advanced-functions/13-project.js
@@ -0,0 +1,104 @@
+const score = JSON.parse(localStorage.getItem("rockpaperscissors"));
+document.querySelector(
+  "#js-score"
+).textContent = `Wins: ${score.wins}, Ties: ${score.ties}, Losses: ${score.losses}`;
+function pickComputerMove() {
+  const randomNumber = Math.floor(Math.random() * 3);
+
+  if (randomNumber === 0) {
+    computerMove = "Rock";
+  } else if (randomNumber === 1) {
+    computerMove = "Paper";
+  } else if (randomNumber === 2) {
+    computerMove = "Scissors";
+  }
+  return computerMove;
+}
+
+let isPlaying = false;
+let intervalId = "";
+
+function autoPlay() {
+  if (!isPlaying) {
+    setInterval(function () {
+      let playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+    isPlaying = true;
+  } else {
+    clearInterval(intervalId);
+    isPlaying = false;
+  }
+}
+
+function playGame(playerMove) {
+  let computerMoveInEmoji = "";
+  const computerMove = pickComputerMove();
+  if (playerMove === "Paper") {
+    if (computerMove === "Rock") {
+      score.wins += 1;
+      result = "Won.";
+      computerMoveInEmoji = "✊";
+    } else if (computerMove === "Paper") {
+      score.ties += 1;
+      result = "Tied.";
+      computerMoveInEmoji = "🖐";
+    } else if (computerMove === "Scissors") {
+      score.losses += 1;
+      result = "Lost.";
+      computerMoveInEmoji = "✌";
+    }
+    playerMove = "🖐";
+    updateScore();
+  } else if (playerMove === "Rock") {
+    if (computerMove === "Rock") {
+      score.ties += 1;
+      result = "Tied.";
+      computerMoveInEmoji = "✊";
+    } else if (computerMove === "Paper") {
+      score.losses += 1;
+      result = "Lost.";
+      computerMoveInEmoji = "🖐";
+    } else if (computerMove === "Scissors") {
+      score.wins += 1;
+      result = "Won.";
+      computerMoveInEmoji = "✌";
+    }
+    playerMove = "✊";
+    updateScore();
+  } else if (playerMove === "Scissors") {
+    if (computerMove === "Rock") {
+      score.losses += 1;
+      result = "Lost.";
+      computerMoveInEmoji = "✊";
+    } else if (computerMove === "Paper") {
+      score.wins += 1;
+      result = "Won.";
+      computerMoveInEmoji = "🖐";
+    } else if (computerMove === "Scissors") {
+      score.ties += 1;
+      result = "Tied.";
+      computerMoveInEmoji = "✌";
+    }
+    playerMove = "✌";
+    updateScore();
+  }
+  document.querySelector(
+    "#js-moves"
+  ).innerHTML = `You <span class="span">${playerMove}</span> <span class="span">${computerMoveInEmoji}</span> Computer`;
+  document.querySelector("#js-result").textContent = `You ${result}`;
+  localStorage.setItem("rockpaperscissors", JSON.stringify(score));
+}
+function resetScore() {
+  (score.wins = 0),
+    (score.losses = 0),
+    (score.ties = 0),
+    (document.querySelector(
+      "#js-score"
+    ).textContent = `Wins: ${score.wins}, Ties: ${score.ties}, Losses: ${score.losses}`);
+}
+function updateScore() {
+  document.querySelector(
+    "#js-score"
+  ).textContent = `Wins: ${score.wins}, Ties: ${score.ties}, Losses: ${score.losses}`;
+}
